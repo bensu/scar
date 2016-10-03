66f464b3a60b064e139411a64f4c2acd33a4a688
Sebastian Bensusan <sbensu@gmail.com>
2016-10-03 01:49:43 -0300

    validate environment with specs

--- environ/src/environ/core.clj
+++ environ/src/environ/core.clj
@@ -1,7 +1,8 @@
 (ns environ.core
   (:require [clojure.edn :as edn]
             [clojure.string :as str]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.spec :as s]))
 
 (defn- keywordize [s]
   (-> (str/lower-case s)
@@ -20,26 +21,94 @@
     (do (println "Warning: environ value" (pr-str v) "for key" k "has been cast to string")
         (str v))))
 
-(defn- read-system-env []
-  (->> (System/getenv)
-       (map (fn [[k v]] [(keywordize k) v]))
-       (into {})))
+(defonce env-specs (atom #{}))
 
-(defn- read-system-props []
-  (->> (System/getProperties)
-       (map (fn [[k v]] [(keywordize k) v]))
+(defn- maybe-keyword [s]
+  (try
+    (keyword s)
+    (catch Throwable _ nil)))
+
+(defn- maybe-read-edn [s]
+  (try
+    (edn/read-string s)
+    (catch Throwable _ s)))
+
+;; maybe (str/replace #"." "-") for Java System properties
+(defn keywordize [s]
+  (-> s
+      str/lower-case
+      (str/replace #"___" "/")
+      (str/replace #"__" ".")
+      (str/replace #"_" "-")
+      maybe-keyword))
+
+(defn- validate-spec! [k v]
+  (when (s/invalid? (s/conform k v))
+    (throw (ex-info (s/explain-str k v) {:key k :val v}))))
+
+(defn- read-configs [source]
+  (->> source
+       (map (fn [[k v]]
+              (let [k' (keywordize k)]
+                (when-let [spec (get @env-specs k')]
+                  (let [v' (maybe-read-edn v)]
+                    (validate-spec! k' v')
+                    [k' v'])))))
        (into {})))
 
 (defn- read-env-file [f]
-  (if-let [env-file (io/file f)]
-    (if (.exists env-file)
-      (into {} (for [[k v] (edn/read-string (slurp env-file))]
-                 [(sanitize-key k) (sanitize-val k v)])))))
+  (when-let [env-file (io/file f)]
+    (when (.exists env-file)
+      (edn/read-string (slurp env-file)))))
 
 (defonce ^{:doc "A map of environment variables."}
   env
   (merge
    (read-env-file ".lein-env")
    (read-env-file (io/resource ".boot-env"))
-   (read-system-env)
-   (read-system-props)))
+   (read-configs (System/getenv))
+   (read-configs (System/getProperties))))
+
+;; API
+
+(s/fdef defenv
+        :args (s/and #(even? (count %))
+                     #(every? keyword? (take-nth 2 %)))
+        )
+
+(defmacro defenv [& specs]
+  `(do
+     ~@(map (fn [[k# s#]]
+              `(do
+                 (swap! env-specs #(conj % ~k#))
+                 (s/def ~k# ~s#)))
+            (partition 2 specs))))
+
+(defonce env-map (atom {}))
+
+(defn validate! []
+  (doseq [k @env-specs]
+    (validate-spec! k (get @env-map k))))
+
+;; TODO: add meta for source name for nice exception messages
+(defn load-env! [source]
+  (swap! env-map #(merge % source))
+  (validate!))
+
+(comment
+
+  ;; each ns declares what it needs
+  (defenv
+    ::port integer?
+    ::db string?)
+
+  ;; somewhere you say what it is
+  (def source-map {::port 3005})
+
+  ;; you initialized
+
+  (load-env! source-map)
+
+  ;; you use the values
+  (env ::port)
+  )
